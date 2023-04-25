7111192c2b3fd8c76acf167888a5aa6538ed4e14
M_gg <103328399+User9990135@users.noreply.github.com>
2023-04-26 02:52:52 +0900

    0426패치
    데이터 저장? 로드 추가 캐릭터의 전체적인 스텟 , 게임 진행상황 저장등등

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,9 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+
+using System.Numerics;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+
+[Serializable]
+public class MyData
+{
+
+    // 게임
+    public int Die;
+//   public int W; //명성
+ //   public int B; //악명
+    public string PlayerName;
+    public int ClearMap;
+    //힘 민첩 지능
+    public int Level;
+    public int H;
+    public int D;
+    public int I;
+    public int Hp;
+    public int Mana;
+
+
+    public void GetData()
+    {
+        Debug.Log("Die : " + Die);
+   //     Debug.Log("W : " + W);
+    //    Debug.Log("B : " + B);
+        Debug.Log("PlayerName : " + PlayerName);
+        Debug.Log("ClearMap : " + ClearMap);
+        //
+        Debug.Log("Level : " + Level);
+        Debug.Log("H : " + H);
+        Debug.Log("D : " + D);
+        Debug.Log("I : " + I);
+        Debug.Log("Hp : " + Hp);
+        Debug.Log("Mana : " + Mana);
+    }
+}
+
 public enum TextKeyWord
 {
-    start,
+    Mstart, //메인 스타트
+    start, //게임스타트
     bettle,
     setting,
     error,
@@ -24,11 +67,70 @@ public class GameManager : MonoBehaviour
     public myInputField myInputfield;
     ////////////////////
     public TextKeyWord textKeyWord;
- 
+    // savedata
+    public int die;
+    public string playername = "아무개";
+    public int clearmap;
+    public int level;
+    public int h ;
+    public int d ;
+    public int  i;
+    public int hp;
+    public int mana;
+
+    public void SaveData()
+    {
+        MyData mydata = new MyData();
+        mydata.Die = die;
+        mydata.PlayerName = playername.ToString();
+        mydata.ClearMap = clearmap;
+        mydata.Level = level;
+        mydata.H = h;
+        mydata.D = d;
+        mydata.I = i;
+        mydata.Hp = hp;
+        mydata.Mana = mana;
+
+        string str = JsonUtility.ToJson(mydata);
+
+        Debug.Log(str);
+
+        File.WriteAllText(Application.persistentDataPath + "/PlayerData.json",
+            JsonUtility.ToJson(mydata));
+        MyData data2 = JsonUtility.FromJson<MyData>(str);
+        data2.GetData();
+
+    }
+
+
+    public void GameLoad(string jsonData)
+    {
+        MyData myData = JsonUtility.FromJson<MyData>(jsonData);
+        myData.GetData();
+        //money = BigInteger.Parse(myData.Money);
+        //weaponLevel = myData.WeaponLevel;
+        //AtkSpd = (float)myData.Atkspd;
+        //stageLevel = myData.StageLevel;
+        die = myData.Die;
+    }
+    //@@@@@@@@@@@@@@@@@@@@@@@@//
+
     void Start()
     {
-        
-        Systemtextprint(TextKeyWord.start);
+        //save try
+        try
+        {
+            string jsonData = File.ReadAllText(Application.persistentDataPath + "/PlayerData.json");
+            // Debug.LogError("test 11");
+            GameLoad(jsonData);// Debug.LogError("test 12");
+        }
+        catch
+        {
+            // Debug.LogError("test 1");
+            SaveData();
+        }
+        //
+        Systemtextprint(TextKeyWord.Mstart);
 
     }
     public void Update()
@@ -53,10 +155,6 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void ABCheck()
-    {
-        //string[] KeyWord = {}
-    }
     public void Systemtextprint(TextKeyWord Snum)
     {
         SystemMessage(Snum);
@@ -69,7 +167,7 @@ public class GameManager : MonoBehaviour
     {
         switch (Num)
         {
-            case TextKeyWord.start:
+            case TextKeyWord.Mstart:
                 Systemtext.text = string.Empty;
                 SystemStr = "반갑습니다\n저는 당신의 게임 시스템 입니다\n[start] [setting] [exit]";
                 break;
@@ -85,10 +183,18 @@ public class GameManager : MonoBehaviour
                 Systemtext.text = string.Empty;
                 SystemStr = "\n안녕하세요.";
                 break;
+            case TextKeyWord.start:
+                Systemtext.text = string.Empty;
+                GameSaveCheck();
+                break;
+
         }
 
     }
-
+    public void GameSaveCheck()
+    {
+        
+    }
     IEnumerator Typing(string text)
     {
         foreach (char letter in text.ToCharArray())
